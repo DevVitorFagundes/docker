6f3083f719deb1ce36ef2e2cf72081bb0ef61239
Vitor Fagundes <vitorfagundes575@gmail.com>
2024-11-25 21:13:33 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def home():
+ 
+ return "Olá, Docker, Aqui Vitor Fagundes!"
+
+
+if __name__ == '__main__':
+ app.run(host='0.0.0.0', port=8080) 
